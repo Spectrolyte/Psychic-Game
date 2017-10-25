254fc30923d0bc88ce29329b2cc9f726654aff66
Diana <dianaltecson@gmail.com>
2017-10-25 07:52:13 -0700

    finished js coding and basic html layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,3 +15,45 @@
 			// increment wins +1
 			// reset guesses to 10
 	// else, alert user to choose a letter
+
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var userGuess = '';
+var computerChoice = '';
+
+function generateCPUChoice () {
+	var randomNumber = Math.floor(Math.random() * letters.length);
+    return letters[randomNumber];
+}
+
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+alert('Press any letter key to start the game!');
+
+document.onkeyup = function game (event) {
+	if (letters.indexOf(event.key) !== -1) {
+		userGuess = event.key;
+		computerChoice =generateCPUChoice();
+		if (guesses === 0) {
+			losses++;
+			guesses = 10;
+			computerChoice =generateCPUChoice();
+		}
+		else if (userGuess !== computerChoice) {
+			guesses--;
+		}
+		else if (userGuess === computerChoice) {
+			wins++;
+			guesses = 10;
+			computerChoice =generateCPUChoice();
+		}
+		document.getElementById('wins').innerHTML = "Wins: " + wins;
+		document.getElementById('losses').innerHTML = "Losses: " + losses;
+		document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
+		document.getElementById('userGuess').innerHTML = "Your Guess: " + userGuess;
+	}
+	else {
+		alert('Press a letter key to play.');
+	}
+}
