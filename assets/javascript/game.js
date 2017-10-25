e807b8f3472f5835982a5a83a9f081730eedb047
Diana <dianaltecson@gmail.com>
2017-10-25 07:01:06 -0700

    added pseudocode for js

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+//create variables: wins, losses, user guess, number of guesses
+	// wins & losses start at zero, increment +1
+	// number of guesses start at 10, decrement -1
+// alert user to press a letter key to start the game
+//on key press
+	// if user chooses letter, start game
+		// letter bank
+		// if guesses = 0
+			// increment losses by +1
+			// computer chooses new letter
+			// guesses reset to 10
+		// else if user guess doesn't match computer guess
+			// guesses decrement -1
+		// else if user guess matches computer guess
+			// increment wins +1
+			// reset guesses to 10
+	// else, alert user to choose a letter
